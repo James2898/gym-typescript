ad6610861aa28e095dee97c6cdf06e4173d67af9
James Martinez <amajamesmartinez@gmail.com>
2023-07-20 23:50:16 +0800

    Home Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/scenes/navbar";
+import Home from "@/scenes/home";
 import * as React from "react";
 import { SelectedPage } from "./shared/types";
 
@@ -29,6 +30,7 @@ function App() {
         selectedPage={selectedPage}
         setSelectedPage={setSelectedPage}
       />
+      <Home setSelectedPage={setSelectedPage} />
     </div>
   );
 }
